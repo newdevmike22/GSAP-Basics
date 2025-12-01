d5cfc5085688c2ab6d597032a9848b7c6e6e92f6
newdevmike22 <paola@dominicansoul.com>
2025-12-01 09:45:13 -0400

    GSAP Timelines
    GSAP timelines method.

---
+++ Starter/Timelines/example one/app.js
@@ -0,0 +1,56 @@
+/*
+const timeLine = gsap.timeline();
+
+timeLine
+  .to(".box1", {
+    duration: 2,
+    scale: 0,
+    opacity: 0,
+    delay: 1,
+  })
+  .to(".box2", {
+    duration: 2,
+    x: -200,
+  })
+  .to(".box3", {
+    duration: 2,
+    x: -200,
+    borderRadius: 0,
+    rotate: 360,
+  })
+  .set(".box1", {
+    duration: 2,
+    scale: 1,
+    opacity: 1,
+  });
+  */
+
+// REFACTOR
+
+const timeLine = gsap.timeline({
+  defaults: {
+    duration: 1,
+  },
+});
+
+timeLine
+  .to(".box1", {
+    scale: 0,
+    opacity: 0,
+    delay: 1,
+  })
+  .to(".box2", {
+    x: -200,
+  })
+  .to(".box3", {
+    x: -200,
+    borderRadius: 0,
+    rotate: 360,
+  })
+  .set(".box1", {
+    x: 200,
+  })
+  .to(".box1", {
+    scale: 1,
+    opacity: 1,
+  });
---
+++ Starter/Timelines/example two/app.js
@@ -0,0 +1,23 @@
+let timeLine = gsap
+  .timeline({
+    default: {
+      duration: 1,
+    },
+  })
+  .to("h1", {
+    y: -100,
+
+    color: "#fff",
+  })
+  .to(".box1", {
+    x: -100,
+    backgroundColor: "pink",
+  })
+  .to(".box2", {
+    y: 100,
+    backgroundColor: "purple",
+  })
+  .to(".box3", {
+    x: 100,
+    backgroundColor: "crimson",
+  });
